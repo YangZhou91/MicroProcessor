2cdefb7c5ba0e48b997414799323157ec2ca15a7
Yang Zhou <gavinzhou10c@gmail.com>
2014-03-14 17:16:50 -0400

    Start working on the accelerometer

---
+++ rtx_project/accelerometer_conf.c
@@ -0,0 +1,37 @@
+#include "stm32f4_discovery_lis302dl.h"
+#include "accelerometer_conf.h"
+#include <stdint.h>
+
+
+/**
+	* @brief Set up configuration for Accelerometer 
+	* @param void
+	*
+	*/
+	void set_Accelerometer_MEM(void)
+	{
+			LIS302DL_InitTypeDef LIS302DL_InitStruct;
+			
+		/* Set configuration for LIS302DL*/
+			LIS302DL_InitStruct.Power_Mode = LIS302DL_LOWPOWERMODE_ACTIVE;
+		/* Set the sampling frequency as 100Hz*/
+			LIS302DL_InitStruct.Output_DataRate = LIS302DL_DATARATE_100;
+		/* Enable X, Y, Z axes*/
+			LIS302DL_InitStruct.Axes_Enable = LIS302DL_XYZ_ENABLE;
+		/* Set the range to +/- 2.0g */
+			LIS302DL_InitStruct.Full_Scale = LIS302DL_FULLSCALE_2_3;
+		/* Set the self-test*/
+			LIS302DL_InitStruct.Self_Test = LIS302DL_SELFTEST_NORMAL;
+			LIS302DL_Init(&LIS302DL_InitStruct);
+
+	}
+	
+	/**
+		* @brief Retrieve data from Accelerometer
+		* @param void
+		*/
+
+	//void LIS302DL_ReadACC(int32_t *output_Accelerometer);
+		
+
+	
--- rtx_project/temperature_thread.c
+++ rtx_project/temperature_thread.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "stm32f4xx.h"
+#include "stm32f4xx_conf.h"
+#include "stm32f4xx_adc.h"
+#include "core_cm4.h"
 #include <cmsis_os.h>
 #include <ADC_Impl.h>
 #include <SevenSegLED.h>
+
 /*!
  @brief Thread to perform detection of temperature 
  @param argument Unused
@@ -13,17 +18,21 @@ void temperature_detection_thread(void const * argument);
 //osThreadDef(temperature_detection_thread, osPriorityNormal, 1, 0);
 
 void temperature_detection_thread (void const *argument) {
-	/*Set configuration */
+	/*Set configuration to ADC for acquiring Vadc*/
 	set_ADC_config();
+	/*Set configuration of LED display for testing purpose*/
+	LED_GPIO_Config();
+	
 	uint16_t Vadc;
 	float temp, Vsense, Vadc_filter, Vdd = 3.0, V25 = 0.76, frequency = 25;
 	
 	while(1){
-		osDelay(1000);
-		
+
 		Vadc = get_ADC_temp();
 		Vsense = Vdd * (float)Vadc / 4095;
 		temp = (Vsense - V25)*(float)(1000/2.5) + (float)25;
+		DisplayTemperature(temp);
+		/*Uncomment this line will break entire project*/
 		//printf("The temperature: %f\n", temp);
 	}
 }
